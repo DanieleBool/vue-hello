44bff74cc8ce88edf8605c4105fd4f7d1308014f
Daniele Di Eleuterio <daniele.dieleuterio@gmail.com>
2021-11-25 20:18:49 +0100

    fine-esercizio

---
+++ script.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+    el: '#container',
+    data: {
+        message: 'Be always like the sea, <br> than breaking up against cliffs it finds always the force to try again.',
+        message2: '(Jim Morrison)',
+        image: 'https://unsplash.it/600/300?image=404'
+
+    }
+});
